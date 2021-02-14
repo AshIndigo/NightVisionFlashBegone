2a12a093a69bac2f54f2b5a8258da0a85d0c960d
Ash <5542898+AshIndigo@users.noreply.github.com>
2021-02-14 16:33:46 +0700

    Updated version to 1.16.5 and cleaned up the code a little bit too so it'll hopefully be nicer with other mods

--- src/main/java/com/ashindigo/nvflashbegone/mixin/NVFlashBeGone.java
+++ src/main/java/com/ashindigo/nvflashbegone/mixin/NVFlashBeGone.java
@@ -1,29 +1,20 @@
 package com.ashindigo.nvflashbegone.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.render.BackgroundRenderer;
-import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.resource.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GameRenderer.class)
 public class NVFlashBeGone {
 
-    @Overwrite()
-    public static float getNightVisionStrength(LivingEntity livingEntity, float f) {
-        int i = livingEntity.getStatusEffect(StatusEffects.NIGHT_VISION).getDuration();
-        return i > 0 ? 1 : i;
+    @Inject(method = "getNightVisionStrength", at = @At("RETURN"), cancellable = true)
+    private static void noFlash_getNightVisionStrength(LivingEntity livingEntity, float f, CallbackInfoReturnable<Float> info) {
+        float i = livingEntity.getStatusEffect(StatusEffects.NIGHT_VISION).getDuration();
+        info.setReturnValue(i > 0 ? 1 : i);
         // i > (time till fades) ? (brightness) : i * (faderate)
     }
 }
